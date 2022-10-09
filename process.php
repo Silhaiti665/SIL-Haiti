7f43f1b33c8d5d9c8dcb89220074ca003b095035
Silhaiti665 <91800038+Silhaiti665@users.noreply.github.com>
2022-10-09 23:22:13 +0200

    Add files via upload

---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$connexion= new PDO('mysql:host=mysql-silhaiti.alwaysdata.net;dbname=silhaiti_membres;charset=utf8','silhaiti_','Blanchard711');
+if(isset($_POST['envoi']));
+  if(!empty($_POST['email']) AND !empty($_POST['password'])){
+
+      $email=htmlspecialchars($_POST['email']);
+      $password=sha1($_POST['password']);
+
+       $recuperusers=$connexion->prepare('SELECT email AND password FROM users WHERE email=? AND password=?');
+       $recuperusers->execute(array($email,$password));
+
+        if($recuperusers->rowCount() > 0){
+			
+		     $_SESSION['email']=$email;
+		     $_SESSION['password']=$password;
+		     
+		     header('Location: '.$uri.'/SIL Haiti/home');
+         
+		}else{
+		     echo "Votre email ou mot de passe  est incorrect";
+		}
+      }else{
+            echo "Veuillez completer tous les champs";
+      }
+?>
+
+
+
+
