8ec1eb2bb571d6a2921f987afd235fa407cc0fdd
Silhaiti665 <91800038+Silhaiti665@users.noreply.github.com>
2022-10-11 05:58:01 +0200

    Update process.php

--- process.php
+++ process.php
@@ -15,7 +15,7 @@ if(isset($_POST['envoi']));
 		     $_SESSION['email']=$email;
 		     $_SESSION['password']=$password;
 		     
-		     header('Location: '.$uri.'/home.html/');
+		     header('Location: '.$uri.'home.html');
          
 		}else{
 		     echo "Votre email ou mot de passe  est incorrect";
